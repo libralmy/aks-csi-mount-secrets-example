4c461665771eb9ddb7bdff0195a726ce6b614c13
emilyli <libralmy1003@gmail.com>
2023-02-01 09:45:28 -0500

    ingress class

--- s5-deployTestPods.sh
+++ s5-deployTestPods.sh
@@ -92,7 +92,7 @@ metadata:
   labels:
     azure.workload.identity/use: "true"
 spec:
-  ingressClassName: $INGRESS_CLASS_NAME # nginx
+  ingressClassName: $INGRESS_CLASS_NAME 
   tls:
   - hosts:
     - $DOMAIN_NAME_FQDN
